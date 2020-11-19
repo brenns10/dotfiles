a5b69f8ddaf495a29b1a40992e017b8e71dc1c15
Stephen Brennan <stephen.s.brennan@oracle.com>
2020-11-19 09:46:30 -0800

    Add doom config

---
+++ .doom.d/config.el
@@ -0,0 +1,73 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Stephen Brennan"
+      user-mail-address "stephen@brennan.io")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
+;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+(setq doom-font (font-spec :family "Inconsolata" :size 14))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-solarized-dark)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+;; Initialize CCLS, specifying that the cache directory be ~/ccls. The after!
+;; is important here.
+(after! ccls
+  (setq ccls-initialization-options `(:cache (:directory ,(expand-file-name "~/ccls"))))
+  (setq ccls-args '("--log-file=/tmp/cc.log" "--log-file-append"))
+  (set-lsp-priority! 'ccls 2)) ; optional as ccls is the default in Doom
+
+;; "highlight" tabs, and include a "mark" (>) for them.
+;; highlight trailing whitespace as well
+(setq whitespace-style '(face tabs tab-mark trailing))
+(setq whitespace-global-modes '(not
+   treemacs-mode magit-status-mode magit-revision-mode vterm-mode
+ ))
+(global-whitespace-mode 1)
+
+;; The Linux Standard Tab WIIIIIDTH
+(setq tab-width 8)
