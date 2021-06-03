1eebd87934f8d7035fd7c8e97325fc8d44bd5db4
Stephen Brennan <stephen@brennan.io>
2021-06-02 19:03:06 -0700

    Revert "Workaround for some issue, I forgot"
    This reverts commit 5d4b7d9907c5353563c6301249f0f331143af0c1.
    
    There's a better one from my work

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -14,7 +14,6 @@
 ;;      Alternatively, press 'gd' (or 'C-c c d') on a module to browse its
 ;;      directory (for easy access to its source code).
 
-(setq evil-respect-visual-line-mode nil)
 (doom! :input
        ;;chinese
        ;;japanese
