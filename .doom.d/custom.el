1deb454edcf5d781f7a0ba25133810aee9643060
Stephen Brennan <stephen@brennan.io>
2021-06-02 18:46:05 -0700

    "safe themes"

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("37144b437478e4c235824f0e94afa740ee2c7d16952e69ac3c5ed4352209eefb" default)))
+   '("08a27c4cde8fcbb2869d71fdc9fa47ab7e4d31c27d40d59bf05729c4640ce834" "37144b437478e4c235824f0e94afa740ee2c7d16952e69ac3c5ed4352209eefb" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
