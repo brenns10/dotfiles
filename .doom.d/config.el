fde36878066e2a022feef7c28572f0b60ebc0678
Stephen Brennan <stephen@brennan.io>
2024-01-23 23:30:40 -0800

    Switch terminal & editor to Source Code Pro
    My yearly oscillation between the two fonts.
    
    Signed-off-by: Stephen Brennan <stephen@brennan.io>

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -21,7 +21,7 @@
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-(setq doom-font (font-spec :family "Inconsolata" :size 14))
+(setq doom-font (font-spec :family "Source Code Pro" :size 13))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
